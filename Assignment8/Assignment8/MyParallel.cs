bd1f4b490bb4fd5bc9793a230e8b80d373077db1
Serob Hayrapetyan <serob.airapetyan@mail.ru>
2019-05-12 21:30:22 +0400

    R

---
+++ Assignment8/Assignment8/MyParallel.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Assignment8
+{
+    class MyParallel
+    {
+        /// <summary>
+        /// My own ForLoop
+        /// </summary>
+        /// <param name="fromInclusive"></param>
+        /// <param name="toExclusive"></param>
+        /// <param name="body"></param>
+        public static void ParallelFor(int fromInclusive, int toExclusive, Action<int> body)
+        {
+            for (; fromInclusive < toExclusive; fromInclusive++)
+            {
+                ThreadPool.QueueUserWorkItem(delegate
+                {
+                    body(fromInclusive);
+                });
+                Thread.Sleep(1);
+            }
+        }
+
+        /// <summary>
+        /// My own ForEachLoop
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="body"></param>
+        public static void ParallelForEach<TSource>(IEnumerable<TSource> source, Action<TSource> body)
+        {
+            foreach (var x in source)
+            {
+                ThreadPool.QueueUserWorkItem(delegate
+                {
+                    body(x);
+                });
+                Thread.Sleep(1);
+            }
+        }
+
+        /// <summary>
+        /// My own ForEachLoopWithOptions5
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="parallelOptions"></param>
+        /// <param name="body"></param>
+        public static void ParallelForEachWithOptions<TSource>(IEnumerable<TSource> source, ParallelOptions parallelOptions, Action<TSource> body)
+        {
+            int maxDeg = parallelOptions.MaxDegreeOfParallelism;
+            if (Environment.ProcessorCount <= maxDeg)
+            {
+                ThreadPool.SetMaxThreads(maxDeg, 10);
+                foreach (var x in source)
+                {
+                    ThreadPool.QueueUserWorkItem(delegate
+                    {
+                        body(x);
+                    });
+                    Thread.Sleep(10);
+                }
+            }
+            else
+            {
+                Console.WriteLine("You have more processors than your thread's count");
+            }
+        }
+
+    }
+}
---
+++ Assignment8/Assignment8/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Assignment8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<int> action;
+            action = Factorial;
+            MyParallel.ParallelFor(0, 10, action); //using my ParallelFor
+
+            int[] arr = new int[] { 2, 5, 9, 7, 8, 4, 3, 10 };
+            MyParallel.ParallelForEach(arr, action);//using my ParallelForEach
+
+            ParallelOptions pr = new ParallelOptions();
+            pr.MaxDegreeOfParallelism = 5;
+            //MyParallel.ParallelForEachWithOptions(arr, pr, Factorial);         
+        }
+        
+        /// <summary>
+        /// Method FActorial for checking ParallelFor
+        /// </summary>
+        /// <param name="x"></param>
+        static void Factorial(int x)
+        {
+            int factorial = 1;
+
+            for (int i = 1; i <= x; i++)
+            {
+                factorial *= i;
+            }
+            Console.WriteLine(" {0} task is going", Task.CurrentId);
+            Console.WriteLine("Factorial of {0} = {1}", x, factorial);
+        }
+    }
+}
