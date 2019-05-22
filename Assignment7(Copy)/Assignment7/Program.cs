b2385d101fc10167aedae4c21d1934900535611c
Serob Hayrapetyan <serob.airapetyan@mail.ru>
2019-05-22 15:56:18 +0400

    This is the .txt file copiing programm.

---
+++ Assignment7(Copy)/Assignment7/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Threading;
+
+namespace Assignment7
+{
+    class Program
+    {
+        public static bool str = true;
+        static void Main(string[] args)
+        {
+            var from = @"C:\Users\Serob\Desktop\HomeworksGit\Assignment7(Copy)\from\copy.txt";
+            var into = @"C:\Users\Serob\Desktop\HomeworksGit\Assignment7(Copy)\in\final.txt";
+
+            Console.WriteLine("Starting copiing the file: ");
+            GetProgAsync(from,into);
+
+            Console.Write("Progress bar - ");
+            
+            while (str)
+            {
+                Console.Write("#");
+                Thread.Sleep(5);
+            }
+            Console.WriteLine("\nCopiing is finished.");
+        }
+        static Task GetProgress(string from,string inTo)
+        {
+            return Task.Run(() =>
+            {
+                File.Copy(from, inTo);
+                str = false;
+            });
+         
+        }
+        static async Task GetProgAsync(string from,string to)
+        {
+            await GetProgress(from, to);
+            //str = false;
+        }
+    }
+}
