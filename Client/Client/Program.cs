42654fc6d28f42dc61c3e054b9164edc3f57bf18
Serob Hayrapetyan <serob.airapetyan@mail.ru>
2019-06-06 20:11:46 +0400

    Client

---
+++ Client/Client/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(new string('_', 55) + "PROGRAMM" + new string('_', 55) + "\n");
+            Console.ResetColor();
+            choose: Console.WriteLine("Please choose which service to use - TCP(1) or UDP(2).If you want to change the protocol, write -other protocol- \n");
+            start: string ch = Console.ReadLine();
+            if (ch == "1")
+            {
+                try
+                {
+                    TcpClient client = new TcpClient();
+                    client.SendTimeout = 1000;
+                    client.Connect("127.0.0.1", 1000);
+                    
+                    byte[] data = new byte[256];
+                    StringBuilder response = new StringBuilder();
+                    NetworkStream stream = client.GetStream();
+
+                    //Connection confirming
+                    do
+                    {
+                        int bytes = stream.Read(data, 0, data.Length);
+                        response.Append(Encoding.UTF8.GetString(data, 0, bytes));
+                    }
+                    while (stream.DataAvailable);
+
+                    Console.WriteLine(response.ToString());
+                    Console.WriteLine("Enter operations in this format : NUMBER space OPERATOR space NUMBER");
+
+                    string message = String.Empty;
+
+                    do
+                    {
+                        if(message == "other protocol")
+                        {
+                            stream.Close();
+                            client.Close();
+                            goto choose;
+
+                        }
+                        message = Console.ReadLine();
+                        byte[] data1 = Encoding.UTF8.GetBytes(message);
+                        stream.Write(data1, 0, data1.Length);
+
+                        StringBuilder result = new StringBuilder();
+                        var data2 = new byte[256];
+                        int bytes = stream.Read(data2, 0, data2.Length);
+                        result.Append(Encoding.UTF8.GetString(data2, 0, bytes));
+                        Console.WriteLine("Result = " + result);
+                    }
+                    while (message != String.Empty);
+
+                    stream.Close();
+                    client.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Something goes wrong...disconnected");
+                }
+            }
+            else if(ch == "2")
+            {
+                Console.Clear();
+
+                Console.WriteLine("WELCOME \nEnter operations in this format : NUMBER space OPERATOR space NUMBER");
+
+                //for sending
+                UdpClient client = new UdpClient();
+                client.Connect("localhost", 1000);
+
+                //for getting
+                UdpClient udpClient = new UdpClient();
+                udpClient.Client.Bind(new IPEndPoint(IPAddress.Any, 1001));
+
+                try
+                {
+                    IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+                    string message = String.Empty;        //this object must be sent to the server
+                    string result = String.Empty;         //this object must be received from server
+
+                    do
+                    {
+                        //Sending to Math server
+                        message = Console.ReadLine();
+
+                        if (message == "other protocol")
+                        {
+                            client.Close();
+                            goto choose;
+                        }
+
+                        var sendBytes = Encoding.ASCII.GetBytes(message);
+                        client.Send(sendBytes, sendBytes.Length);
+
+                        //Receiving result from math server
+                        var receiveBytes = udpClient.Receive(ref RemoteIpEndPoint);
+                        result = Encoding.ASCII.GetString(receiveBytes);
+                        Console.WriteLine("Result = " + result);
+                    }
+                    while (message != String.Empty);
+
+                    client.Close();
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("Something goes wrong...disconnected");
+                    return;
+                }
+            }
+            else
+            {
+                goto start;
+            }
+        }
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MATH SERVER");
+
+            //I divided the process into 2 threads
+            Thread tr1 = new Thread(TcpProt);
+            Thread tr2 = new Thread(UdpProt);
+            tr1.Start();
+            tr2.Start();
+            
+
+        }
+
+        /// <summary>
+        /// This method uses TPS protocol
+        /// </summary>
+        public static void TcpProt()
+        {
+            try
+            {
+                TcpListener server = new TcpListener(IPAddress.Parse("127.0.0.1"), 1000);
+                server.Start();
+
+                Console.WriteLine("Waiting for connection... ");
+
+                //Getting coming connection
+                TcpClient tcpClient = server.AcceptTcpClient();
+                Console.WriteLine("Client is connected,doing queary...");
+
+                //getting network stream for reading and writing
+                NetworkStream stream = tcpClient.GetStream();
+
+                //COnfirming message
+                string response = "CONNECTED";
+                byte[] data1 = Encoding.UTF8.GetBytes(response);
+
+                //sending message
+                stream.Write(data1, 0, data1.Length);
+
+                do
+                {
+                    StringBuilder message = new StringBuilder();
+                    byte[] data = new byte[256];
+
+                    int bytes = stream.Read(data, 0, data.Length);
+                    message.Append(Encoding.UTF8.GetString(data, 0, bytes));
+                    string messageString = message.ToString();
+                    Console.WriteLine(messageString);
+
+                    if (messageString == "exit")
+                    {
+                        break;
+                    }
+
+                    string send;
+
+                    string tiv1 = null;
+                    string tiv2 = null;
+                    int indexOfOperator = 0;
+                    
+                    if (messageString != String.Empty)
+                    {
+                        for (int i = 0; i < message.Length; i++)
+                        {
+                            if (message[i] == ' ')
+                            {
+                                indexOfOperator = i + 1;
+                                for (int k = 0; k < i; k++)
+                                {
+                                    tiv1 = tiv1 + message[k];
+                                }
+                                for (int k = i + 3; k < message.Length; k++)
+                                {
+                                    tiv2 = tiv2 + message[k];
+                                }
+                                break;
+                            }
+                        }
+                    }
+
+                    double result = 0;
+
+                    switch (message[indexOfOperator])
+                    {
+                        case '+':
+                            result = Convert.ToInt32(tiv1) + Convert.ToInt32(tiv2);
+                            break;
+                        case '-':
+                            result = Convert.ToInt32(tiv1) - Convert.ToInt32(tiv2);
+                            break;
+                        case '*':
+                            result = Convert.ToInt32(tiv1) * Convert.ToInt32(tiv2);
+                            break;
+                        case '/':
+                            result = Convert.ToInt32(tiv1) / Convert.ToInt32(tiv2);
+                            break;
+                    }
+                    Console.WriteLine("Worked");
+
+                    send = Convert.ToString(result);
+
+                    byte[] data2 = Encoding.UTF8.GetBytes(send);
+                    stream.Write(data2, 0, data2.Length);
+                }
+                while (stream.CanRead);
+
+                stream.Close();
+                // закрываем подключение
+                tcpClient.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Something goes wrong...disconnected");
+                return;
+            }
+        }
+
+        /// <summary>
+        /// This method uses UDP protocol
+        /// </summary>
+        public static void UdpProt()
+        {
+            //for getting
+            UdpClient udpClient = new UdpClient();
+            udpClient.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            udpClient.Client.Bind(new IPEndPoint(IPAddress.Any, 1000));
+
+            //for sending
+            UdpClient client = new UdpClient();
+            client.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            client.Connect("localhost", 1001);
+            try
+            {
+                IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 1);
+
+                string message = String.Empty;
+                do
+                {
+                    var receiveBytes = udpClient.Receive(ref RemoteIpEndPoint);
+                    message = Encoding.ASCII.GetString(receiveBytes);
+                    Console.WriteLine(message);
+                    string tiv1 = null;
+                    string tiv2 = null;
+                    int indexOfOperator = 0;
+                    if (message != String.Empty)
+                    {
+                        for (int i = 0; i < message.Length; i++)
+                        {
+                            if (message[i] == ' ')
+                            {
+                                indexOfOperator = i + 1;
+                                for (int k = 0; k < i; k++)
+                                {
+                                    tiv1 = tiv1 + message[k];
+                                }
+                                for (int k = i + 3; k < message.Length; k++)
+                                {
+                                    tiv2 = tiv2 + message[k];
+                                }
+                                break;
+                            }
+                        }
+                    }
+
+                    double result = 0;
+
+                    switch (message[indexOfOperator])
+                    {
+                        case '+':
+                            result = Convert.ToInt32(tiv1) + Convert.ToInt32(tiv2);
+                            break;
+                        case '-':
+                            result = Convert.ToInt32(tiv1) - Convert.ToInt32(tiv2);
+                            break;
+                        case '*':
+                            result = Convert.ToInt32(tiv1) * Convert.ToInt32(tiv2);
+                            break;
+                        case '/':
+                            result = Convert.ToInt32(tiv1) / Convert.ToInt32(tiv2);
+                            break;
+                    }
+                    Console.WriteLine("Result = " + result);
+
+                    var sendBytes = Encoding.ASCII.GetBytes(Convert.ToString(result));
+                    client.Send(sendBytes, sendBytes.Length);
+                }
+                while (message != "exit");
+                udpClient.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Something goes wrong...disconnected");
+                return;
+            }
+        }
+    }
+}
